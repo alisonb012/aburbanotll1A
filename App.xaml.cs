00a62202c1424be70d365ce358ec6b2a7f200253
alisonb012 <e1752153856@uisrael.edu.ec>
2025-05-02 16:00:28 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace aburbanotll1A
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new Views.vPrincipal());
+        }
+    }
+}
---
+++ Views/vPrincipal.xaml.cs
@@ -0,0 +1,68 @@
+using System.Text.RegularExpressions;
+
+namespace aburbanotll1A.Views;
+
+public partial class vPrincipal : ContentPage
+{
+	public vPrincipal()
+	{
+		InitializeComponent();
+	}
+    private void btnGuardar_Clicked(object sender, EventArgs e)
+    {
+        string nombre = txtNombre.Text;
+        string identificacion = txtIdentificacion.Text;
+        string correo = txtCorreo.Text;
+        string confirmarCorreo = txtCorreoConfirmacion.Text;
+        string apellido = txtApellido.Text;
+        string telefono = txtTelefono.Text;
+        string contenido = $"Nombre: {nombre}\n" +
+                   $"Apellido: {apellido}\n" +
+                   $"Identificación: {identificacion}\n" +
+                   $"Correo: {correo}\n" +
+                   $"Teléfono: {telefono}\n";
+
+
+        if (string.IsNullOrEmpty(nombre) || string.IsNullOrWhiteSpace(identificacion) || string.IsNullOrWhiteSpace(correo) || string.IsNullOrWhiteSpace(confirmarCorreo) || string.IsNullOrEmpty(telefono))
+        {
+            DisplayAlert("Error", "Por favor ingrese todos los campos ", "Ok");
+        }
+
+        if (!Regex.IsMatch(nombre ?? "", @"^[a-zA-Z\s]+$"))
+        {
+            lblResultado.Text = "Nombre solo debe contener letras.";
+            return;
+        }
+
+        if (!Regex.IsMatch(identificacion ?? "", @"^\d+$"))
+        {
+            lblResultado.Text = "Cédula solo debe contener números.";
+            return;
+        }
+
+        if (correo != confirmarCorreo)
+        {
+            lblResultado.Text = "Los correos no coinciden.";
+            return;
+        }
+
+        if (!Regex.IsMatch(apellido ?? "", @"[a-zA-Z\s]+$"))
+        {
+            lblResultado.Text = "Nombre solo debe contener letras.";
+            return;
+        }
+
+        try
+        {
+            // Guardar en archivo de texto
+            string ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Datos.txt");
+            File.AppendAllText(ruta, contenido); // Agrega al final del archivo
+            lblResultado.Text = $"Datos guardados correctamente en:\n{ruta}";
+        }
+        catch (Exception ex)
+        {
+            lblResultado.Text = $"Error al guardar: {ex.Message}";
+        }
+    }
+
+    }
